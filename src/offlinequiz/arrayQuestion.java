05a44aa03921db2d0a1ceb83647c296b4470b487
Nikniknikm <46752236+Nikniknikm@users.noreply.github.com>
2019-01-18 09:12:09 +0100

    Add files via upload

---
+++ src/offlinequiz/arrayQuestion.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package offlinequiz;
+
+import static java.awt.AWTEventMulticaster.add;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author nikamahani
+ */
+public class arrayQuestion {
+
+    int answer = 4;
+    int question = 20;
+
+    String[][] myArray = new String[question][answer];
+
+    public void readFile(String filePath) throws FileNotFoundException {
+
+        int j = 0;
+        int i = 0;
+        Scanner s = new Scanner(new File(filePath));
+        s.useDelimiter(";");
+        String line;
+        while (s.hasNext()) {
+            while (i < 20) {
+                line = s.next();
+                myArray[i][j] = line;
+                while (j < 3) {
+                    line = s.next(); // Checka filen
+                    j++;
+                    myArray[i][j] = line;
+
+                }
+                i++;
+                j = 0;
+
+            }
+
+        }
+
+    }
+
+    public String[][] getArray() {
+        return Arrays.copyOf(myArray, myArray.length);
+    }
+}
